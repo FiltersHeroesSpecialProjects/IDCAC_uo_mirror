bbe495bdfd99f01e4cc5a16d28a3461acd785f70
Κρυςτιαν3ϐ <35370833+krystian3w@users.noreply.github.com>
2025-01-03 19:04:25 +0100

    Update updateMirror.sh
    49 nie wydaje się być dopuszczone

--- updateMirror.sh
+++ updateMirror.sh
@@ -4,7 +4,7 @@ MAIN_PATH=$(git -C "$(dirname "$(realpath -s "$1")")" rev-parse --show-toplevel)
 cd "$MAIN_PATH" || exit
 mkdir -p ./src
 cd ./src || exit
-wget "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&acceptformat=crx3&x=id%3Dfihnjjcciajhdojfnbdddfaoknhalnja%26installsource%3Dondemand%26uc" -O "IDCAC.crx"
+wget "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=99.0&acceptformat=crx3&x=id%3Dfihnjjcciajhdojfnbdddfaoknhalnja%26installsource%3Dondemand%26uc" -O "IDCAC.crx"
 unzip -o ./IDCAC.crx
 rm -rf ./IDCAC.crx
 rm -rf ./_metadata
